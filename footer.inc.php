685e65aa1da7310f6a2018e414c1e013db68598c
Rick Stender <rick@hypenotic.com>
2014-09-26 21:47:43 -0400

    linkedin button
    Add new linkedin button to footer

--- footer.inc.php
+++ footer.inc.php
@@ -4,5 +4,8 @@
 		<li><a href="<?php get_site_url(); ?>sitemap/">Sitemap</a></li>
 	</ul>
 	<p>&copy; Barefoot Brainstorming <?php echo date("Y") ?>. All rights reserved.</p>
-	<p><a href="http://www.linkedin.com/company/barefoot-brainstorming?trk=fc_badge"><img src="http://static01.linkedin.com/scds/common/u/img/webpromo/btn_cofollow_badge.png" locale="en" alt="Barefoot Brainstorming on LinkedIn"></a></p>
+	<p><a href="http://www.linkedin.com/company/barefoot-brainstorming?trk=fc_badge"><!--<img src="http://static01.linkedin.com/scds/common/u/img/webpromo/btn_cofollow_badge.png" locale="en" alt="Barefoot Brainstorming on LinkedIn">--><script src="//platform.linkedin.com/in.js" type="text/javascript">
+  lang: en_US
+</script>
+<script type="IN/FollowCompany" data-id="1932397" data-counter="none"></script></a></p>
 </div>
