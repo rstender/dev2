d2d00cbb581d8dc971da8fe6ac76dea353263745
Rick Stender <rick@hypenotic.com>
2014-10-01 03:05:28 -0400

    replace header with includes

--- homepage.php
+++ homepage.php
@@ -16,28 +16,13 @@
 <body id="<?php return_page_slug(); ?>" >
 
 <!-- Begin Header -->
-<div id="header_wrap">
-   <div id="header">
-      <div id="logo">
-         <a href="<?php get_site_url(); ?>"><img src="<?php get_theme_url(); ?>/images/barefoot_logo_blue.gif" width="204" height="206" alt="Barefoot Brainstorming" /></a>
-      </div>
-      <div id="main_navigation">
-	         <ul>
-	            <li><a href="<?php get_site_url(); ?>" id="home">Home</a></li>
-	            <li><a href="<?php get_site_url(); ?>who-is-barefoot/" id="who">Who is Barefoot?</a></li>
-	            <li><a href="<?php get_site_url(); ?>our-services/" id="services">Our Services</a></li>
-	            <li><a href="<?php get_site_url(); ?>why-go-barefoot/" id="why">Why Go Barefoot?</a></li>
-	            <li><a href="<?php get_site_url(); ?>our-clients/" id="clients">Our Clients</a></li>
-	            <li><a href="<?php get_site_url(); ?>in-the-news/" id="news">In the News</a></li>
-	            <li><a href="<?php get_site_url(); ?>contact-us/" id="contact">Contact Us</a></li>
-	         </ul>
-      </div>
-		<div id="testimonial_header">
-			<a href="<?php get_site_url(); ?>our-clients/testimonials/"><img src="<?php get_theme_url(); ?>/images/testimonial_home.jpg" width="968" height="162" /></a>
-		</div>
-   </div>
-</div>
+	<?php include('header.inc.php'); ?>
 <!-- End Header -->
+
+<!-- Begin Header Responsive -->
+	<?php include('header-responsive.inc.php'); ?>
+<!-- End Header Responsive -->
+
 
 <div id="wrapper">
 <!-- Begin Content -->
